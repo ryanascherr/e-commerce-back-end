9971c6f9ae6bf358120d4260918c58be56e20390
ryanascherr <ryanascherr@gmail.com>
2021-05-16 10:13:47 -0400

    Defined columns for category model.

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,14 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+CREATE TABLE category(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+)
+CREATE TABLE product(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(50) NOT NULL,
+    
+    PRIMARY KEY (id)
+)
